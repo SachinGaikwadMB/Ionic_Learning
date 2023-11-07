8894cc5ddfa4c8ca21e9bbbe58dc886fbf4c6fb4
sachin-gaikwad <sachin.gaikwad@mindbowser.com>
2023-11-07 11:11:13 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Hello_World_App',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
